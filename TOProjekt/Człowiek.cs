5fbb2588d8bc101d2671f4bfecfe3ec6618d8d93
Aithlin <wikita228@gmail.com>
2017-01-14 15:53:46 +0100

    baza danych pacjentow - singleton

--- TOProjekt/Człowiek.cs
+++ TOProjekt/Człowiek.cs
@@ -8,9 +8,20 @@ namespace TOProjekt
 {
     public class Człowiek
     {
-        string imie;
-        string nazwisko;
-        string telefon;
+        public string imie;
+        public string nazwisko;
+        public string telefon;
 
+        public Człowiek()
+        {
+
+        }
+
+        public Człowiek(string imie, string nazwisko, string telefon)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.telefon = telefon;
+        }
     }
 }
---
+++ TOProjekt/Kartoteka.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOProjekt
+{
+    class Kartoteka
+    {
+        public HashSet<Pacjent> pacjenci = new HashSet<Pacjent>();
+
+
+        private static Kartoteka kartoteka;
+
+        private Kartoteka() { }
+
+        public static Kartoteka getInstance()
+        {
+            if(kartoteka == null)
+            {
+                kartoteka = new Kartoteka();
+            }
+
+            return kartoteka;
+        }
+    }
+}
