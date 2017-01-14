21e86a385978cd8e1b64e101e1c75f1fd8478e28
Aithlin <wikita228@gmail.com>
2017-01-14 18:47:34 +0100

    sprawdzenie poprawnosci danych na przykladzie lekarzy

--- TOProjekt/Program.cs
+++ TOProjekt/Program.cs
@@ -11,6 +11,16 @@ namespace TOProjekt
         static void Main(string[] args)
         {
             ustawDane();
+            try
+            {
+                sprawdzDane();
+            }
+            catch(Exception e)
+            {
+                System.Environment.Exit(1);
+            }
+
+            sprawdzDane();
 
             Rejestracja.telefon("Dupencjusz", "Pierdzioch", "Pulmunolog");
             Rejestracja.telefon("Dupencjusz", "Robak", "Kardiolog");
@@ -23,6 +33,37 @@ namespace TOProjekt
 
             //---------------------LEKARZE------------------------
             Kartoteka.getInstance().pulmunolodzy.Add(new Pulmunolog("Ronald", "Marek"));
+        }
+
+        static private void sprawdzDane()
+        {
+            //---------------------LEKARZE------------------------
+            if (Kartoteka.getInstance().dermatolodzy.Count() < 1)
+            {
+                System.Console.WriteLine("Nie został stworzony żaden dermatolog");
+                throw new Exception();
+            }
+            if (Kartoteka.getInstance().kardiolodzy.Count() < 1)
+            {
+                System.Console.WriteLine("Nie został stworzony żaden kardiolog");
+                throw new Exception();
+            }
+            if (Kartoteka.getInstance().laryngolodzy.Count() < 1)
+            {
+                System.Console.WriteLine("Nie został stworzony żaden laryngolog");
+                throw new Exception();
+            }
+            if (Kartoteka.getInstance().okulisci.Count() < 1)
+            {
+                System.Console.WriteLine("Nie został stworzony żaden okulista");
+                throw new Exception();
+            }
+            if (Kartoteka.getInstance().pulmunolodzy.Count() < 1)
+            {
+                System.Console.WriteLine("Nie został stworzony żaden pulmunolog");
+                throw new Exception();
+            }
+                
         }
     }
 }
