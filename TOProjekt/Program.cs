007a345e49f5c8333988cb60bfc51423fe647982
Aithlin <wikita228@gmail.com>
2017-01-23 19:54:56 +0100

    zagubiony plik

--- TOProjekt/Program.cs
+++ TOProjekt/Program.cs
@@ -32,40 +32,40 @@ namespace TOProjekt
         static private void ustawDane()
         {
             //---------------------PACJENCI-----------------------
-            Kartoteka.getInstance().pacjenci.Add(new Pacjent("Dupencjusz", "Robak"));
+            Kartoteka.Instance.pacjenci.Add(new Pacjent("Dupencjusz", "Robak"));
 
             //---------------------LEKARZE------------------------
-            Kartoteka.getInstance().lekarze.Add(new Dermatolog("Alfred", "Zieba"));
-            Kartoteka.getInstance().lekarze.Add(new Kardiolog("Henryk", "Zajac"));
-            Kartoteka.getInstance().lekarze.Add(new Laryngolog("Rufus", "Mysior"));
-            Kartoteka.getInstance().lekarze.Add(new Okulista("Bohdan", "Fredzel"));
-            Kartoteka.getInstance().lekarze.Add(new Pulmunolog("Ronald", "Marek"));
+            Kartoteka.Instance.lekarze.Add(new Dermatolog("Alfred", "Zieba"));
+            Kartoteka.Instance.lekarze.Add(new Kardiolog("Henryk", "Zajac"));
+            Kartoteka.Instance.lekarze.Add(new Laryngolog("Rufus", "Mysior"));
+            Kartoteka.Instance.lekarze.Add(new Okulista("Bohdan", "Fredzel"));
+            Kartoteka.Instance.lekarze.Add(new Pulmunolog("Ronald", "Marek"));
         }
 
         static private void sprawdzDane()
         {
             //---------------------LEKARZE------------------------
-            if (Kartoteka.getInstance().dermatolodzy.Count() < 1)
+            if (Kartoteka.Instance.dermatolodzy.Count() < 1)
             {
                 System.Console.WriteLine("Nie został stworzony żaden dermatolog");
                 throw new Exception();
             }
-            if (Kartoteka.getInstance().kardiolodzy.Count() < 1)
+            if (Kartoteka.Instance.kardiolodzy.Count() < 1)
             {
                 System.Console.WriteLine("Nie został stworzony żaden kardiolog");
                 throw new Exception();
             }
-            if (Kartoteka.getInstance().laryngolodzy.Count() < 1)
+            if (Kartoteka.Instance.laryngolodzy.Count() < 1)
             {
                 System.Console.WriteLine("Nie został stworzony żaden laryngolog");
                 throw new Exception();
             }
-            if (Kartoteka.getInstance().okulisci.Count() < 1)
+            if (Kartoteka.Instance.okulisci.Count() < 1)
             {
                 System.Console.WriteLine("Nie został stworzony żaden okulista");
                 throw new Exception();
             }
-            if (Kartoteka.getInstance().pulmunolodzy.Count() < 1)
+            if (Kartoteka.Instance.pulmunolodzy.Count() < 1)
             {
                 System.Console.WriteLine("Nie został stworzony żaden pulmunolog");
                 throw new Exception();
