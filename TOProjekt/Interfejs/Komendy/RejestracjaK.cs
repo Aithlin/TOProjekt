284df9d8282cf48882866c2984cd62e90f187ea4
Aithlin <wikita228@gmail.com>
2017-01-28 17:57:15 +0100

    Rejestracja 4 argumenty

--- TOProjekt/Interfejs/Komendy/RejestracjaK.cs
+++ TOProjekt/Interfejs/Komendy/RejestracjaK.cs
@@ -8,18 +8,34 @@ namespace TOProjekt.Interfejs.Komendy
 {
     public class RejestracjaK:IKomenda
     {
+        bool czyBlad = false;
         string imie, nazwisko, lekarz;
 
-        public RejestracjaK(string imie, string nazwisko, string lekarz) 
+        public RejestracjaK(IEnumerable<string> polecenie) 
         {
-            this.imie = imie;
-            this.nazwisko = nazwisko;
-            this.lekarz = lekarz;
+            if(polecenie.Count() != 4)
+            {
+                czyBlad = true;
+            }
+            else
+            {
+                this.imie = polecenie.ElementAtOrDefault<string>(1);
+                this.nazwisko = polecenie.ElementAtOrDefault<string>(2);
+                this.lekarz = polecenie.ElementAtOrDefault<string>(3);
+            }
         }
 
         public bool wykonaj()
         {
-            Rejestracja.telefon(imie, nazwisko, lekarz);
+            if(czyBlad)
+            {
+                Console.WriteLine("Nieprawidlowa ilosc argumentow, poprawna skladnia: rejestracja imie nazwisko lekarz");
+            }
+            else
+            {
+                Rejestracja.telefon(imie, nazwisko, lekarz);
+            }
+
             return false;
         }
     }
